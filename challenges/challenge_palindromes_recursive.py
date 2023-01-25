c48f4709605779a88556672daa5523f4fd840110
soficia <let942@gmail.com>
2023-01-25 17:31:02 -0300

    feat: 3: palíndromo recursivo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,8 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word[0] != word[-1] or len(word) == 0:
+        return False
+    if word[0] == word[-1]:
+        word = word[1:-1]
+        if len(word) == 1 or len(word) == 0:
+            return True
+        return is_palindrome_recursive(word)
