8ca6fe2f97bd49204fc016f2d9bfbf2441eed6fd
soficia <let942@gmail.com>
2023-01-25 15:56:30 -0300

    feat: 1 Algoritmo de busca de estudantes no mesmo horário

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,10 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+    if type(target_time) != int or target_time == 0:
+        return None
+    for tupla in permanence_period:
+        if type(tupla[0]) != int or type(tupla[1]) != int:
+            return None
+        if tupla[0] <= target_time and tupla[1] >= target_time:
+            count += 1
+    return count
