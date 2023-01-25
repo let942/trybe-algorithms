dfb5c4b3b15fa57df4be255c948e0efeaf488716
soficia <let942@gmail.com>
2023-01-25 18:40:39 -0300

    refactor: 3: palíndromo recursivo

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,8 +1,12 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    if word[0] != word[-1] or len(word) == 0:
+    if len(word) == 0:
+        return False
+    if word[0] != word[-1]:
         return False
     if word[0] == word[-1]:
         word = word[1:-1]
         if len(word) == 1 or len(word) == 0:
             return True
-        return is_palindrome_recursive(word)
+        low_index = word[0]
+        high_index = word[-1]
+        return is_palindrome_recursive(word, low_index, high_index)
