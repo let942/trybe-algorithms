5ff7a12613589b2716191dd7904974d80e9a371e
soficia <let942@gmail.com>
2023-01-25 22:29:36 -0300

    style: retira link dos comentários

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -11,7 +11,7 @@ def is_anagram(first_string, second_string):
         boolean = False
     return ("".join(first_word), "".join(second_word), boolean)
 
-# retirado merge_sort do course:https://app.betrybe.com/learn/course/5e938f69-6e32-43b3-9685-c936530fd326/module/290e715d-73e3-4b2d-a3c7-4fe113474070/section/73f2a1d5-7d77-440b-a9f2-3ea9750db228/day/346e18ae-25d8-47a5-bd59-0e4d9cd2a2d2/lesson/9b22b10c-1e8a-4f00-bab8-edac69573b6b
+# retirado merge_sort do course
 
 
 def merge_sort(numbers, start=0, end=None):
