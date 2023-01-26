eb5223dffa60646191d1242df4363f8eadf03d5a
soficia <let942@gmail.com>
2023-01-26 15:04:01 -0300

    feat: 5: Encontra números repetidos

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,33 @@
+def find_duplicate2(nums):
+    dic = {}
+    for num in nums:
+        if type(num) == int and num > 0 and num in dic:
+            dic[num] += 1
+        else:
+            dic[num] = 1
+    for element in dic:
+        if dic[element] > 1:
+            return element
+    return False
+
+
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(nums) < 2:
+        return False
+    duplicated = duplicated_or_none(nums)
+    if duplicated is None:
+        return False
+    return duplicated
+
+
+def duplicated_or_none(nums):
+    uniq_nums = set()
+    duplicated = None
+    for num in nums:
+        if type(num) != int or num < 1:
+            return None
+        if num in uniq_nums:
+            duplicated = num
+        else:
+            uniq_nums.add(num)
+    return duplicated
